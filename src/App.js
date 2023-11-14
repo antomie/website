8e28b99282b9e15b8d65e491f21e303659b771c6
antomie <antonytran04@gmail.com>
2023-11-13 19:19:02 -0600

    finished skills

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
+import Skills from "./components/Skills/skills";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Intro/>
+      <Skills/>
     </div>
   );
 }
